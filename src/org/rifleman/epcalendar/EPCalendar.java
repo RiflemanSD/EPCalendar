d08d139b73996ad574edbdec282c7cae035f33a4
RiflemanSD <sotirisdoudis@windowslive.com>
2016-03-08 00:05:25 +0200

    Initial Commit

---
+++ src/org/rifleman/epcalendar/EPCalendar.java
@@ -0,0 +1,22 @@
+
+package org.rifleman.epcalendar;
+
+/**
+ * <h1>EPCalendar</h1>
+ * <h3>Class EPCalendar<h3> 
+ * <p>Created: 8 Μαρ 2016, 12:04:06 πμ</p></br>
+ *
+ * <p>Copyright © 2015 | RiflemanSD | All right reserved</p>
+ *
+ * @author RiflemanSD
+ */
+public class EPCalendar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
